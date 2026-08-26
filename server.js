d0bd72413e6152adcb2a08a2b1a09ce22361b4fe
tiago233tek-spec <tiago233tek@gmail.com>
2026-08-26 00:58:42 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Demenor Sensi online na porta ${PORT}`);
+});
